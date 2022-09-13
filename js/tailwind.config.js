b1548e0cb21349edcfce1493f4e6a5fc7c658d25
asifislam2 <111923685+asifislam2@users.noreply.github.com>
2022-09-13 23:50:08 +0600

    first Commit

---
+++ js/tailwind.config.js
@@ -0,0 +1,26 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          clifford: '#da373d',
+          red: 'red',
+          hover: '#FAFF14',
+        },
+
+        maxWidth: {
+            contanier: '1000px',
+        },
+        fontFamily:{
+
+            popins:'Poppins',
+
+        },
+
+        backgroundImage:{
+
+            banner: "url('images/banner.png')",
+        }
+        
+      }
+    }
+  }
